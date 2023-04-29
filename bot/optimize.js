84a0d50cba23c19cbcac77f373552612d751ff7f
jocoly <jclyell@iu.edu>
2023-04-29 19:32:13 -0400

    added buttons to optimize response

--- bot/optimize.js
+++ bot/optimize.js
@@ -1,5 +1,7 @@
+import {ActionRowBuilder, ButtonBuilder, ButtonStyle} from "discord.js";
 import {optimizationStringArray} from "./optimizationStringArray.js";
-import {openai} from "./bot.js";
+import {client, openai} from "./bot.js";
+import {commands, prefixes} from "./commands.js";
 
 export async function optimize(msg) {
     let prompt = msg.content.replace(/^!(borpaoptimize|optimize|borptimize)\s*/, "");
@@ -12,5 +14,48 @@ export async function optimize(msg) {
         temperature: Number(process.env.CHAT_TEMPERATURE)
     });
     console.log(optimizationStringArray[randomIndex])
-    msg.reply(completion.data.choices[0].text)
+
+    const draw = new ButtonBuilder().setCustomId('draw').setLabel('Draw this').setStyle(ButtonStyle.Success);
+    const cancel = new ButtonBuilder().setCustomId('cancel').setLabel('Cancel').setStyle(ButtonStyle.Danger);
+    const row = new ActionRowBuilder().addComponents(draw, cancel);
+
+    const reply = await msg.reply({
+        content: completion.data.choices[0].text,
+        components: [row],
+    })
+
+    const filter = (interaction) => {
+        // Check if the interaction is from the user who received the reply
+        return interaction.customId === 'draw' || interaction.customId === 'cancel' && interaction.user.id === msg.author.id;
+    };
+
+    const collector = reply.createMessageComponentCollector({filter, time: 15000});
+
+    collector.on('collect', async (interaction) => {
+        if(!interaction.isButton()) return;
+
+        if (interaction.customId === 'draw') {
+            await msg.channel.send(prefixes.std+commands.draw+" "+completion.data.choices[0].text);
+            await interaction.update({
+                content: 'Submitting prompt...',
+                components: []
+            });
+        }
+        else if (interaction.customId === 'cancel') {
+            await interaction.update({
+                content: 'Canceled.',
+                components: []
+            });
+        }
+        collector.stop();
+    })
+
+    collector.on('end', async (collected) => {
+        // Disable the buttons after the collector ends
+        if (collected.size === 0) {
+            await reply.edit({
+                components: [],
+            });
+        }
+    });
 }
